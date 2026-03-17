c47a4fd33852aac1584447a7210b3db07ec3792e
clement-hervouet <clement.hervouet57@laposte.net>
2026-03-17 16:40:23 +0100

    UPDATE : modification de la colonne is_admin à role dans la table users

--- web/database/sql.sql
+++ web/database/sql.sql
@@ -10,5 +10,5 @@ CREATE TABLE users (
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    is_admin BOOLEAN DEFAULT FALSE
+    role     ENUM('admin','lecteur','editeur') DEFAULT 'lecteur'
 );
